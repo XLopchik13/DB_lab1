5592f146253f6a7256e8fd8f636c1efd3f3fd7a2
XLopchik13 <nikita.petrov.it@gmail.com>
2023-10-03 23:07:08 +0300

    inter_lib

--- db_main.py
+++ db_main.py
@@ -16,27 +16,33 @@ class Variant:
         self.path = path
 
 
-print("          students")
-print("id name    surname    patronymic")
-
+students_list = []
+for i in range(50):
+    students_list.append([])
 n = 1
+i = 0
 with open('names.txt', encoding="utf8") as f_names:
     for line in f_names:
         a = line.split()
         student = Student(n, a[0], a[1], a[2])
-        print(student.id, student.name, student.surname, student.patronymic, sep='  ')
+        #students_list[i].append(student.id, student.name, student.surname, student.patronymic)
+        students_list[i].append(student.id)
+        students_list[i].append(student.name)
+        students_list[i].append(student.surname)
+        students_list[i].append(student.patronymic)
         n += 1
+        i += 1
 
 with open('variants.txt', encoding="utf8") as f_variants:
     for line in f_variants:
         a = line.split()
         variant = Variant(a[0], a[1])
-        print(variant.id, variant.path)
+        #print(variant.id, variant.path)
 
+print(students_list)
 root = Tk()
 root.title("Teacher program")
 root.geometry("800x400")
-people = [("Tom", 38, "tom@email.com"), ("Bob", 42, "bob@email.com"), ("Sam", 28, "sam@email.com")]
 columns = ("id", "name", "surname", "patronymic")
 tree = ttk.Treeview(columns=columns, show="headings")
 tree.pack(fill=BOTH, expand=1)
@@ -44,7 +50,7 @@ tree.heading("id", text="id")
 tree.heading("name", text="name")
 tree.heading("surname", text="surname")
 tree.heading("patronymic", text="patronymic")
-for person in people:
+for person in students_list:
     tree.insert("", END, values=person)
 root.mainloop()
 
