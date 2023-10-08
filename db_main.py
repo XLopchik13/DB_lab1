d0e63112e1559832d11a01073890ac1d7d8c5405
XLopchik13 <nikita.petrov.it@gmail.com>
2023-10-09 00:20:45 +0300

    funcs

--- db_main.py
+++ db_main.py
@@ -2,65 +2,77 @@ from tkinter import *
 from tkinter import ttk
 from functions import *
 
-titles = []
-tables_list = []
+titles = []  # названия столбцов
+table_list = []  # список таблиц
 generated_table = {}
-tables_names = []
+table_names = []  # названия таблиц
 cur_table = ''
 
 print("Добро пожаловать в систему оценивания студентов!", 'Чтобы увидеть команды, введите /help', sep='\n')
 cmd = 0
 while cmd != '/e':
     cmd = input().split()
-    if cmd == '/help':
-        print("new table: /nt #table_name []")
+    if cmd[0] == '/help':
+        print("new table: /nt #table_name #surname-name-midname")
         print("select table: /st #table_name")
+        print("add row: /add #surname-name-midname")
         print("print table: /pt #table_name")
-        print("add row: /add #surname_name_midname")
         print("delete row: /dl #row_number")
         print("print row: /pr #row_number")
         print("edit cell: /edc #row #column #new_data")
         print("clear cell: /dc #row #column")
         print("exit: /e")
     elif cmd[0] == '/nt':
-        if len(cmd) != 2:
-            print("wrong command")
-        else:
-            new_table(titles, tables_list, cmd[1])
+        new_table(titles, table_list, table_names, cmd)
+
     elif cmd[0] == '/st':
-        if len(cmd) != 2:
-            print("wrong command")
+        if len(cmd) == 2 and cmd[1] in table_names:
+            cur_table = cmd[1]
+            print('selected "', cur_table, '"', sep='')
         else:
-            pass
+            print("wrong command")
+
+
+    elif cmd[0] == '/add':
+        if cur_table != '':
+            add_row(table_names, table_list, cur_table, cmd)
+        else:
+            print("table not selected")
+
     elif cmd[0] == '/pt':
         if len(cmd) != 2:
             print("wrong command")
         else:
-            pass
-    elif cmd[0] == '/add':
-        pass
+            print_table()
+
     elif cmd[0] == '/dl':
         if len(cmd) != 2:
             print("wrong command")
         else:
             pass
+
     elif cmd[0] == 'pr':
         if len(cmd) != 2:
             print("wrong command")
         else:
             pass
+
     elif cmd[0] == 'edc':
         if len(cmd) != 4:
             print("wrong command")
         else:
             pass
+
     elif cmd[0] == 'dc':
         if len(cmd) != 3:
             print("wrong command")
         else:
             pass
-    elif cmd == '/e':
+
+    elif cmd[0] == '/e':
         print("o kurwa")
+        break
+
     else:
         print("wrong command")
 
--- functions.py
+++ functions.py
@@ -1,2 +1,16 @@
-def new_table(titles, tables_list, name):
+def new_table(titles, table_list, table_names, cmd):
+    index = 0
+    titles.append(cmd[2::])
+    table_list.append({})
+    table_names.append(cmd[1])
+    print(titles, table_list, table_names)
+
+
+def add_row(table_names, table_list, cur_table, cmd):
+    ind = table_names.index(cur_table)
+    table_list[ind][len(table_list[ind])+1] = cmd[1::]
+    print(table_list)
+
+
+def print_table():
     pass
